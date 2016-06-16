620ad1631bd8dd4b33a2aa421ea08b34250cbee4
Ricardo Loureiro <ricardo@devsparkle.me>
2016-06-16 10:42:25 +0100

    Cleaned Bundles
    Removed comments

--- OPOMUN/App_Start/BundleConfig.vb
+++ OPOMUN/App_Start/BundleConfig.vb
@@ -1,7 +1,6 @@
 ﻿Imports System.Web.Optimization
 
 Public Module BundleConfig
-	' For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 	Public Sub RegisterBundles(ByVal bundles As BundleCollection)
 		bundles.Add(
 			New ScriptBundle("~/Bundles/jQuery").Include(
