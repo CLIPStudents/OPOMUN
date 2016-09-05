c4a96b26dcfa0feda7767eedb603f373737fd8b4
Ricardo Loureiro <ricardo@devsparkle.me>
2016-09-05 15:27:41 +0100

    Disabled EnableOptimizations

--- OPOMUN/App_Start/BundleConfig.vb
+++ OPOMUN/App_Start/BundleConfig.vb
@@ -3,12 +3,12 @@
 Public Module BundleConfig
 	Public Sub RegisterBundles(ByVal bundles As BundleCollection)
 
-		''// Style bundles
+		' Style bundles
 		bundles.Add(New StyleBundle("~/Content/Bundles/CoreStyle").Include(
 					"~/Content/CSS/bulma.css",
 					"~/Content/CSS/MUNStyle.css"))
 
-		''// Toggle minification and bundling during Debug
-		BundleTable.EnableOptimizations = True
+		' Toggle minification and bundling during Debug
+		'BundleTable.EnableOptimizations = True
 	End Sub
 End Module
