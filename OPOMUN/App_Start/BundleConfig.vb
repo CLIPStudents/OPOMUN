5d3de871781f682740b7d6bbfc39dc1c45ba14a6
Ricardo Loureiro <ricardo@devsparkle.me>
2016-09-15 14:26:43 +0100

    Initial clean commit

---
+++ OPOMUN/App_Start/BundleConfig.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Web.Optimization
+
+Public Module BundleConfig
+	Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+
+	End Sub
+End Module
---
+++ OPOMUN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="OPOMUN.MvcApplication" Language="VB" %>
