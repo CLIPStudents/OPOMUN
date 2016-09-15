51cdda733734a91d88b629bb59017d329444ac6b
Ricardo Loureiro <ricardo@devsparkle.me>
2016-09-15 15:26:36 +0100

    Implemented previous Frameworks
    Implemented previosly used frameworks, as mentioned in the issue #2 and
    created placeholders for CoreStyle

--- OPOMUN/App_Start/BundleConfig.vb
+++ OPOMUN/App_Start/BundleConfig.vb
@@ -2,6 +2,15 @@
 
 Public Module BundleConfig
 	Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+		' Scripts
+		bundles.Add(New ScriptBundle("~/Content/Bundles/jQuery").Include(
+					"~/Content/Scripts/jquery-{version}.js"))
 
+		bundles.Add(New ScriptBundle("~/Content/Bundles/CoreScripts").Include(
+					"~/Content/Scripts/bulma.js"))
+
+		' Styles
+		bundles.Add(New StyleBundle("~/Content/Bundles/CoreStyle").Include(
+				  "~/Content/CSS/bulma.css"))
 	End Sub
 End Module
