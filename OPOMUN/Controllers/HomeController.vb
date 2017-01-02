517f06bc3ea981b2efc6b2bc71a69e2b35766d16
Ricardo Loureiro <ricardo@devsparkle.me>
2017-01-02 17:26:25 +0000

    Added Committees master page
    Started work on #25

--- OPOMUN/Controllers/HomeController.vb
+++ OPOMUN/Controllers/HomeController.vb
@@ -11,6 +11,36 @@ Public Class HomeController
 		Return View()
 	End Function
 
+	Function Committee(ByVal Id As String) As ActionResult
+		Dim Model As New MasterPage
+		Dim SubpageContainerPath = "~/Views/Home/CommitteePartials/"
+		Dim SubpagePartials = Directory.GetFiles(Server.MapPath(SubpageContainerPath))
+		Dim IsFirst = True
+
+		For Each SubpagePartial In SubpagePartials
+			Dim IsActive = False
+			If Id IsNot Nothing And IO.File.Exists(Server.MapPath(SubpageContainerPath & Id & ".vbhtml")) Then
+				If Id = Path.GetFileNameWithoutExtension(SubpagePartial) Then
+					IsActive = True
+				End If
+			ElseIf IsFirst Then
+				IsActive = True
+			End If
+			IsFirst = False
+
+			If IsActive Then
+				Model.PartialToLoad = SubpageContainerPath & Path.GetFileName(SubpagePartial)
+			End If
+
+			Model.Subpages.Add(New NavSubpage With {
+				.Name = Path.GetFileNameWithoutExtension(SubpagePartial),
+				.IsActive = IsActive
+			})
+		Next
+
+		Return View(Model)
+	End Function
+
 	Function About(ByVal Id As String) As ActionResult
 		Dim Model As New MasterPage
 		Dim SubpageContainerPath = "~/Views/Home/AboutPartials/"
